32410446e2029a5b8d5ea4c214da14c1a042005a
George Anderson <george.anderson@fiveonefour.com>
2024-09-06 06:17:11 -0700

    update moosetest

--- moosetest/app/datamodels/models.ts
+++ moosetest/app/datamodels/models.ts
@@ -5,57 +5,76 @@ export const LogsConfig: DataModelConfig<Logs> = {
     format: IngestionFormat.JSON,
   },
   storage: {
-    enabled: true,
+    enabled: false,
     order_by_fields: ["resourceLogs"],
   },
 };
 
-export interface Logs {
-  resourceLogs: {
-    resource: {
-      attributes: {
-        key: string;
-        value: {
-          stringValue: string;
-        };
-      }[];
-      droppedAttributesCount: number;
+interface Attribute {
+  key: string;
+  value: {
+    stringValue: string;
+  };
+}
+
+interface LogRecord {
+  timeUnixNano: string;
+  observedTimeUnixNano: string;
+  severityNumber: number;
+  severityText: string;
+  body: {
+    value: {
+      stringValue: string;
     };
-    scopeLogs: {
-      scope: {
-        name: string;
-        version: string;
-        attributes: {
-          key: string;
-          value: {
-            stringValue: string;
-          };
-        }[];
-        droppedAttributesCount: number;
-      };
-      logRecords: {
-        timeUnixNano: string;
-        observedTimeUnixNano: string;
-        severityNumber: number;
-        severityText: string;
-        body: {
-          value: {
-            stringValue: string;
-          };
-        };
-        attributes: {
-          key: string;
-          value: {
-            stringValue: string;
-          };
-        }[];
-        droppedAttributesCount: number;
-        flags: number;
-        traceId: string;
-        spanId: string;
-      }[];
-      schemaUrl: string;
-    }[];
-    schemaUrl: string;
-  }[];
+  };
+  attributes: Attribute[];
+  droppedAttributesCount: number;
+  flags: number;
+  traceId: string;
+  spanId: string;
+}
+
+interface ScopeLog {
+  scope: {
+    name: string;
+    version: string;
+    attributes: Attribute[];
+    droppedAttributesCount: number;
+  };
+  logRecords: LogRecord[];
+  schemaUrl: string;
+}
+
+interface ResourceLog {
+  resource: {
+    attributes: Attribute[];
+    droppedAttributesCount: number;
+  };
+  scopeLogs: ScopeLog[];
+  schemaUrl: string;
+}
+
+export interface Logs {
+  resourceLogs: ResourceLog[];
+}
+
+export const ParsedLogsConfig: DataModelConfig<ParsedLogs> = {
+  ingestion: {
+    format: IngestionFormat.JSON,
+  },
+  storage: {
+    enabled: true,
+    order_by_fields: ["date"],
+  },
+};
+
+export interface ParsedLogs {
+  date: Date;
+  message: string;
+  severityNumber: number;
+  severityLevel: string;
+  source: string;
+  sessionId: string;
+  serviceName: string;
+  machineId: string;
 }
